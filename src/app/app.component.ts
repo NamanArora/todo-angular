acfdef9a36f8efd0e2c084a4b95f23201f3926a6
Naman Arora <namanarora11@gmail.com>
2016-12-23 16:15:26 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import {TodoService} from './todo.service';
+import {Todo} from './todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  providers: [TodoService],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Todo app!';
+  list: Todo[];
+  todo:Todo = new Todo();
+
+  constructor(private todoService : TodoService)
+  {
+    this.fetch();
+  }
+
+  delete(id: number)
+  {
+    this.todoService.deleteTodo(id);
+    this.fetch();
+  }
+
+  addTodo(): void
+  {
+    this.todoService.addTodo(this.todo);
+    this.todo = new Todo();
+  }
+  fetch()
+  {
+    this.list = this.todoService.getAllTodo();
+  }
+
 }
---
+++ src/app/todo.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {Todo} from './todo';
+
+
+@Injectable()
+export class TodoService {
+
+  id: number=0;
+  todos: Todo[] = [];
+
+  constructor() {  }
+
+  addTodo(todo: Todo)
+  {
+    todo.id = this.id+1;
+    this.id+=1;
+    this.todos.push(todo);
+  }
+
+  getAllTodo(): Todo[]
+  {
+    return this.todos;
+  }
+
+  deleteTodo(id: number)
+  {
+    this.todos = this.todos.filter(data => data.id !== id);
+  }
+
+}
